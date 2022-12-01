6c71ad05dda6dedc83db5bc73764da553e8e725e
arun.jayarajan <L00171224@atu.ie>
2022-12-01 18:23:31 +0000

    first commit

---
+++ src/test/java/com/example/javamavenjunithelloworld/AppTest.java
@@ -0,0 +1,17 @@
+package com.example.javamavenjunithelloworld;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
